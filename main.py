b4c807f5fbcd300f1741ab904e94f3b61ba79f44
Chrollo <seanchakra@gmail.com>
2026-02-08 19:59:18 -0600

    E: Made the json file and set fps max to 60.

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ from logger import log_state
 
 
 def main():
+    pygame.init()
     version = pygame.version.ver
     print(f"Starting Asteroids with pygame version: {version}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+    
+
 
     while True:
-        log_state
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
             pass
+
         screen.fill("black")
         pygame.display.flip()
+        dt = Clock.tick(60)
+        dt= dt/1000
+        print(dt)
+
 
 
 
